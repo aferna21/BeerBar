08f7ad1c6af82e1c3bf6063eb18a75f21e831f0a
Adrián Fernández <aferna21@estudiantes.unileon.es>
2019-01-04 21:11:45 +0100

    Metodos y documentacion clase Transaccion

--- src/modelo/Transaccion.java
+++ src/modelo/Transaccion.java
@@ -6,4 +6,96 @@ public class Transaccion {
     private Fecha fecha;
     private Usuario usuario;
     private boolean esVenta;
+
+
+
+    public Transaccion(float cantidad, String concepto, Fecha fecha, Usuario usuario){
+        this.cantidad = cantidad;
+        this.concepto = concepto;
+        this.fecha = fecha;
+        this.usuario = usuario;
+        if(cantidad >= 0) {
+            this.esVenta = true;
+        } else {
+            this.esVenta = false;
+        }
+    }
+
+    //cantidad
+
+    public float getCantidad(){
+        return this.cantidad
+    }
+
+    public void setCantidad(float c){
+        this.cantidad = c;
+    }
+
+
+    //concepto
+
+    public String getConcepto(){
+        return this.concepto;
+    }
+
+    public void setConcepto(String c){
+        this.concepto = c;
+    }
+
+
+    //fecha
+
+    public Fecha getFecha(){
+        return this.fecha;
+    }
+
+    public void setFecha(Fecha f){
+        this.fecha = f;
+    }
+
+
+    //usuario
+
+    public Usuario getUsuario(){
+        return this.usuario;
+    }
+
+    public void setUsuario(Usuario u){
+        this.usuario = u;
+    }
+
+
+    //esVenta
+
+    public boolean getEsVenta(){
+        return this.esVenta;
+    }
+
+    public void setEsVenta(boolean b){
+        this.esVenta = b;
+    }
+
+
+    public String toString(){
+        return new String();
+    }
+
+
+    /**
+     * Devuelve true si el usuario es el que ha creado la transaccion.
+     * @param usuario presunto creador de la transaccion.
+     * @return true si el usuario es el que ha escrito la transaccion.
+     */
+    public boolean esUsuario(Usuario usuario){
+        return false;
+    }
+
+    /**
+     * Devuelve true si la transaccion esta escrita en una fecha determinada
+     * @param fecha presunta fecha en la que ha sido escrita la transaccion
+     * @return true si es la fecha de escritura de la transaccion
+     */
+    public boolean estaEscritaEn(Fecha fecha){
+        return false;
+    }
 }
