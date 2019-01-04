0e023201adcb54743ab2a7feae14f7172fd83ca3
Adrián Fernández <aferna21@estudiantes.unileon.es>
2019-01-04 20:27:02 +0100

    Metodos y documentacion clase Jornada

--- src/modelo/Jornada.java
+++ src/modelo/Jornada.java
@@ -1,6 +1,44 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Jornada {
     private Fecha fecha;
-    private Transaccion[] transacciones;
+    private ArrayList<Transaccion> transacciones;
+
+    public Fecha getFecha(){
+        return this.fecha;
+    }
+
+    public ArrayList<Transaccion> getTransacciones(){
+        return this.transacciones;
+    }
+
+    public void setFecha(Fecha fecha){
+        this.fecha = fecha;
+    }
+
+    public String toString(){
+        return new String();
+    }
+
+    //ESTE METODO NO SALE EN EL DIAGRAMA. ANADIR EXCEPCION
+
+    /**
+     * Anade una transaccion mas al array de transacciones de la jornada.
+     * @param transaccion transaccion anadida a la jornada.
+     */
+    public void anadirTransaccion(Transaccion transaccion){
+        this.transacciones.add(transaccion);
+    }
+
+
+    /**
+     * Devuelve el sumatorio de las transacciones de la jornada.
+     * @return sumatorio de las transacciones en esta jornada.
+     */
+    public int sumaTransacciones(){
+        return 0;
+    }
+
 }
