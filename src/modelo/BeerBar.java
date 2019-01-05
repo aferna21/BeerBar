06ea7da7d02aeca0924c9a1767614692074ea908
Falvarezmn <falvam00@estudiantes.unileon.es>
2019-01-05 19:53:05 +0100

    Añadidos metodos clase BeerBar.

--- src/modelo/BeerBar.java
+++ src/modelo/BeerBar.java
@@ -1,6 +1,61 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class BeerBar {
+
     private Jornada jornadas;
     private GestorDeUsuarios usuarios;
+
+    public BeerBar(){
+
+    }
+
+    public void iniciarSesion(Usuario usuario){
+
+    }
+
+    public void escribirNota(Usuario usuarioOrigen, Usuario usuarioDestino, String texto){
+
+    }
+
+    public ArrayList verHistorialDeTransacciones(){
+        return new ArrayList();
+    }
+
+    public boolean eliminarUsuario(){
+        return true;
+    }
+
+    public boolean anadirUsuario(){
+        return true;
+    }
+
+    public ArrayList verNotasDeTodosLosUsuarios(){
+        return new ArrayList();
+    }
+
+    public boolean modificarTransaccion(Transaccion transaccion, int cantidad){
+        return true;
+    }
+
+    public boolean introducirTransaccion(boolean esVenta, int cantidad){
+        return true;
+    }
+
+    public ArrayList verNotasRecibidas(){
+        return new ArrayList();
+    }
+
+    public boolean descargarCopiaDeSeguridad(){
+        return true;
+    }
+
+    public void consultarAyuda(){
+
+    }
+
+    public boolean cerrarSesion(Usuario usuario){
+        return true;
+    }
 }
