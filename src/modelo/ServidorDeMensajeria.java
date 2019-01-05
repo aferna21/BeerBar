712a034f051ad7d50a7dbab3ce76aa51857d998e
Falvarezmn <falvam00@estudiantes.unileon.es>
2019-01-05 20:19:22 +0100

    Añadidos métodos clase ServidorDeMensajería.

--- src/modelo/ServidorDeMensajeria.java
+++ src/modelo/ServidorDeMensajeria.java
@@ -1,8 +1,24 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class ServidorDeMensajeria {
 
     private Nota[] nota;
 
+    public ArrayList mostrarNotasDe(Usuario usuario){
+        return new ArrayList();
+    }
 
+    public ArrayList mostrarNotasHacia(Usuario usuario){
+        return new ArrayList();
+    }
+
+    public ArrayList mostrarTodasNotas(Usuario usuario){
+        return new ArrayList();
+    }
+
+    public ArrayList mostrarNotasDelDia(Fecha fecha){
+        return new ArrayList();
+    }
 }
