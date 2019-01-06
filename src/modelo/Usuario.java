d0c30269410d187b618bcd9efbc9738fba8c45f6
Falvarezmn <falvam00@estudiantes.unileon.es>
2019-01-06 17:13:54 +0100

    Creado el Singleton de ServidorDeMensajeria y algun otro cambio.

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -2,21 +2,36 @@ package modelo;
 
 import java.util.ArrayList;
 
+/**
+ * Maneja los usuarios del sistema. Sabe de ellos su nombre de usuario, contrasena y si
+ * es administrador o no. El administrador tiene mas permisos que el resto.
+ */
 public class Usuario {
 
+    /**
+     * Nombre del Usuario.
+     */
     private String nombre;
+
+    /**
+     * Contrasena que le permite acceder al sistema.
+     */
     private String contrasena;
+
+    /**
+     * True solo si es el administrador.
+     */
     private boolean esAdmin;
 
 
 
     /**
      * Constructor de la clase. Crea un usuario y le asigna el nombre, la
-     * contrasena y si es administrador o no
+     * contrasena y si es administrador o no.
      *
-     * @param nombre - Nombre del nuevo usuario
-     * @param contrasena - Contrasena del nuevo usuario
-     * @param esAdmin - Si es administrador o no
+     * @param nombre - Nombre del nuevo usuario.
+     * @param contrasena - Contrasena del nuevo usuario.
+     * @param esAdmin - True si es el administrador.
      */
     public Usuario(String nombre, String contrasena, boolean esAdmin)
     {
@@ -24,6 +39,10 @@ public class Usuario {
         this.contrasena = contrasena;
         this.esAdmin = esAdmin;
     }
+
+    /**
+     * Getters y setters.
+     */
 
     public String getNombre(){
         return this.nombre;
@@ -39,10 +58,10 @@ public class Usuario {
 
     /**
      * Crea una nueva nota y se la envia al usuario destino (las notas son manejadas por
-     * el servidor de mensajes)
+     * el servidor de mensajes).
      *
-     * @param usuario - usuario destino
-     * @param nota - Texto de la nota
+     * @param usuario - Usuario destino.
+     * @param nota - Texto de la nota.
      */
     public void escribirNotaA(Usuario usuario, String nota){
 
