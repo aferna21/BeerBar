680dfb1e6064cede05b634dfa5eee96865a916f2
Adrián Fernández <aferna21@estudiantes.unileon.es>
2019-01-07 21:07:14 +0100

    error de compilacion

--- test/modelo/CalendarioTest.java
+++ test/modelo/CalendarioTest.java
@@ -10,7 +10,7 @@ public class CalendarioTest {
 
     @Before
     public void setUp() throws Exception{
-        calendario = new Calendario();
+        //calendario = new Calendario();
     }
 
     @Test
