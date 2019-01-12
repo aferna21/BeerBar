c754b3e39f2a1bb68a827f535cf154705cfdf598
Falvarezmn <falvam00@estudiantes.unileon.es>
2019-01-12 12:46:58 +0100

    INTERFAZ DE USUARIO ACABADA.

--- src/vista/JTextAreaTransacciones.java
+++ src/vista/JTextAreaTransacciones.java
@@ -4,13 +4,13 @@ import javax.swing.*;
 
 public class JTextAreaTransacciones extends JTextArea {
 
-    public JTextAreaTransacciones(Boolean esVenta, String usuario, String fecha, String importe){
+    public JTextAreaTransacciones(Boolean esVenta, String usuario, String fecha, String importe, String concepto){
 
         if(esVenta){
-            this.setText("----------\nTipo: Venta" + "\nUsuario: " + usuario + "\nFecha: " + fecha + "\nImporte: " + importe + "\n----------\n");
+            this.setText("----------\nTipo: Venta" + "\nUsuario: " + usuario + "\nFecha: " + fecha + "\nImporte: " + importe + "\nConcepto: " + concepto + "\n----------\n");
         }
         else{
-            this.setText("----------\nTipo: Gasto" + "\nUsuario: " + usuario + "\nFecha: " + fecha + "\nImporte: " + importe + "\n----------\n");
+            this.setText("----------\nTipo: Gasto" + "\nUsuario: " + usuario + "\nFecha: " + fecha + "\nImporte: " + importe + "\nConcepto: " + concepto +  "\n----------\n");
         }
         this.setEditable(false);
     }
--- src/vista/VentanaTransaccionesUsuario.java
+++ src/vista/VentanaTransaccionesUsuario.java
@@ -43,16 +43,57 @@ public class VentanaTransaccionesUsuario extends JFrame {
         //INTRODUCIR VENTA
         panelIntroducirVenta = new JPanel();
         panelIntroducirVenta.setSize(800, 600);
+        panelIntroducirVenta.setLayout(new BorderLayout());
+        JPanel panelVenta = new JPanel();
+        panelVenta.setLayout(new BoxLayout(panelVenta, BoxLayout.PAGE_AXIS));
+        panelIntroducirVenta.add(panelVenta, BorderLayout.NORTH);
+        JLabel textoImporteVenta = new JLabel("Importe de la venta");
+        JTextField campoTextoImporteVenta = new JTextField();
+        JLabel textoConceptoVenta = new JLabel("Concepto");
+        panelVenta.add(textoImporteVenta);
+        panelVenta.add(campoTextoImporteVenta);
+        panelVenta.add(textoConceptoVenta);
+        JTextArea campoConceptoVenta = new JTextArea();
+        panelIntroducirVenta.add(campoConceptoVenta, BorderLayout.CENTER);
+        JButton botonIntroducirVenta = new JButton("Introducir venta");
+        panelIntroducirVenta.add(botonIntroducirVenta, BorderLayout.SOUTH);
+        panelIntroducirVenta.setBorder(new EmptyBorder(new Insets(10,10, 10, 10)));
 
 
         //INTRODUCIR GASTO
         panelIntroducirGasto = new JPanel();
         panelIntroducirGasto.setSize(800, 600);
+        panelIntroducirGasto.setLayout(new BorderLayout());
+        JPanel panelGasto = new JPanel();
+        panelGasto.setLayout(new BoxLayout(panelGasto, BoxLayout.PAGE_AXIS));
+        panelIntroducirGasto.add(panelGasto, BorderLayout.NORTH);
+        JLabel textoImporteGasto = new JLabel("Importe del gasto");
+        JTextField campoTextoImporteGasto = new JTextField();
+        JLabel textoConceptoGasto = new JLabel("Concepto");
+        panelGasto.add(textoImporteGasto);
+        panelGasto.add(campoTextoImporteGasto);
+        panelGasto.add(textoConceptoGasto);
+        JTextArea campoConceptoGasto = new JTextArea();
+        panelIntroducirGasto.add(campoConceptoGasto, BorderLayout.CENTER);
+        JButton botonIntroducirGasto = new JButton("Introducir gasto");
+        panelIntroducirGasto.add(botonIntroducirGasto, BorderLayout.SOUTH);
+        panelIntroducirGasto.setBorder(new EmptyBorder(new Insets(10,10, 10, 10)));
 
 
         //VER TRANSACCIONES
         panelVerTransacciones = new JPanel();
         panelVerTransacciones.setSize(800, 600);
+        panelVerTransacciones.setLayout(new BorderLayout());
+        JPanel panelDentroScrollTransacciones = new JPanel();
+        panelDentroScrollTransacciones.setLayout(new BoxLayout(panelDentroScrollTransacciones, BoxLayout.PAGE_AXIS));
+        panelDentroScrollTransacciones.setBorder(new EmptyBorder(new Insets(10, 10, 10, 10)));
+        JScrollPane panelScrollTransacciones = new JScrollPane(panelDentroScrollTransacciones);
+        panelScrollTransacciones.getVerticalScrollBar().setUnitIncrement(40);
+        panelVerTransacciones.add(panelScrollTransacciones, BorderLayout.CENTER);
+        panelDentroScrollTransacciones.setBackground(Color.BLACK);
+
+        panelDentroScrollTransacciones.add(new JTextAreaTransacciones(true, "Felipe", "1 de Enero de 2019", "24,57", "Unos buenos cocacolos con una tapichuela de tortilluca"));
+        panelDentroScrollTransacciones.add(new JTextAreaTransacciones(false, "Adri", "2 de Enero de 2019", "13,56", "Compra de un buen kilo de alioli como el de hasan"));
 
 
         opcionIntroducirVenta.addActionListener(new ActionListener() {
