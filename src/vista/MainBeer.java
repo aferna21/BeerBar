61ec2daf431fc8b0f6fde4f51cd362fd6b877c78
Adrián Fernández <aferna21@estudiantes.unileon.es>
2019-01-15 14:42:10 +0100

    Funciona el main pero no asigna bien la hija.

--- src/vista/MainBeer.java
+++ src/vista/MainBeer.java
@@ -10,15 +10,15 @@ public class MainBeer {
 
         new VentanaBienvenida();
 
-        /*new ControladorInicio().actualizarUsuarios();
+        new ControladorInicio().actualizarUsuarios();
 
         new DAONotas().introduceNota("Felipe", "Adri", "1/4/2019", null, null, "Primera nota");
         ServidorDeMensajeria.darInstancia().setNotas(new DAONotas().devuelveNotas());
 
-        new DAONotas().introduceNota("Adri", "Felipe", "2/4/2019", ServidorDeMensajeria.darInstancia().mostrarNotasDe(GestorDeUsuarios.darInstancia().getUsuario("Adri")).get(0), null, "Segunda nota");
+        new DAONotas().introduceNota("Adri", "Felipe", "2/4/2019", ServidorDeMensajeria.darInstancia().mostrarNotasDe(GestorDeUsuarios.darInstancia().getUsuario("Felipe")).get(0), null, "Segunda nota");
         ServidorDeMensajeria.darInstancia().setNotas(new DAONotas().devuelveNotas());
 
-        new DAONotas().introduceNota("Felipe", "Adri", "3/4/2019", ServidorDeMensajeria.darInstancia().mostrarNotasDe(GestorDeUsuarios.darInstancia().getUsuario("Felipe")).get(0), ServidorDeMensajeria.darInstancia().mostrarNotasDe(GestorDeUsuarios.darInstancia().getUsuario("Adri")).get(1), "Tercera nota");
-        ServidorDeMensajeria.darInstancia().setNotas(new DAONotas().devuelveNotas());*/
+        new DAONotas().introduceNota("Felipe", "Adri", "3/4/2019", ServidorDeMensajeria.darInstancia().mostrarNotasDe(GestorDeUsuarios.darInstancia().getUsuario("Felipe")).get(0), ServidorDeMensajeria.darInstancia().mostrarNotasDe(GestorDeUsuarios.darInstancia().getUsuario("Adri")).get(0), "Tercera nota");
+        ServidorDeMensajeria.darInstancia().setNotas(new DAONotas().devuelveNotas());
     }
 }
